cccc5d9d4a807ee9ecc179ea413e79b7a74d16f3
Neil Delmer <neil.delmer@gmail.com>
2017-10-27 16:53:40 -0400

    fix error

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  scope :api, defaults: {format: :json} do
+    resources :foos, except: [:new, :edit]
+    resources :bars, except: [:new, :edit]
+  end
+
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root "ui#index"
+end
