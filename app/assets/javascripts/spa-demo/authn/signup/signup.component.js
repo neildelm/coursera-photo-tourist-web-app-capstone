dfdcd378496931244e8bc6d41bf34e543815043e
Neil Delmer <neil.delmer@gmail.com>
2017-12-01 11:48:01 -0500

    Fix image upload

--- app/assets/javascripts/spa-demo/authn/signup/signup.component.js
+++ app/assets/javascripts/spa-demo/authn/signup/signup.component.js
@@ -61,9 +61,9 @@
       console.log("setImageContent", dataUri ? dataUri.length : null);
       vm.image = new Image();
       vm.image.image_content = DataUtils.getContentFromDataUri(dataUri);
-      if dataUri.length {
+      if (dataUri.length) {
         vm.image.image_content.content_type = "image/jpeg";
-      }  
+      }
       console.log("setImageContent", vm.image);
 
     }
